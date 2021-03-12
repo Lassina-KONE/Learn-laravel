fe752f107c05429b60ed5bd6ce1b9a4c9ad4cc48
Lassina-KONE <58139655+Lassina-KONE@users.noreply.github.com>
2021-03-12 21:22:24 +0000

    use service providers & repositories

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ImagesRequest;
+use App\Repositories\ImagesRepositoryInterface;
 
 class ImagesController extends Controller
 {
@@ -11,10 +12,11 @@ class ImagesController extends Controller
         return view('photo');
     }
  
-    public function store(ImagesRequest $request)
+    public function store(ImagesRequest $request, ImagesRepositoryInterface $imagesRepository )
     {
-        $request->image->store(config('images.path'), 'public');
          
+        $imagesRepository->save($request->image);
+
         return view('photo_uploaded');
     }
 }
---
+++ app/Repositories/ImagesRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+ 
+namespace App\Repositories;
+ 
+use Illuminate\Http\UploadedFile;
+ 
+interface ImagesRepositoryInterface
+{
+  public function save(UploadedFile $image);
+}
